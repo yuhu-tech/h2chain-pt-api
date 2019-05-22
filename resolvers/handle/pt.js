df4f5fb754fabb7ac8ed7b39f38e694a065b2482
lutianzhou001 <lutianzhou@yuhu.tech>
2019-05-22 14:30:03 +0800

    checkout a early version that we will not judge if the orderOrigin.lenght is null

--- resolvers/handle/pt.js
+++ resolvers/handle/pt.js
@@ -184,9 +184,6 @@ async function PTGetOrderList(ctx, initialid, id, orderid, datetime) {
         request.setStatus(2)
         var response = await queryOrder(request);
         var res = JSON.parse(response.array[0])
-        if (res.orderOrigins.length == 0) {
-          throw new Error ("cannot find order, is the order closed?")
-        }
         for (var i = 0; i < res.orderOrigins.length; i++) {
             var obj = {}
 
