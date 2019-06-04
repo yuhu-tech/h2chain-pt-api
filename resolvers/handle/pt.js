931adc8db00dc0d1eb629f7a4a2e318296e34f47
lutianzhou001 <lutianzhou@yuhu.tech>
2019-06-04 11:14:37 +0800

    throw an error if cannot find the order

--- resolvers/handle/pt.js
+++ resolvers/handle/pt.js
@@ -187,6 +187,9 @@ async function PTGetOrderList(ctx, initialid, id, orderid, datetime) {
         request.setStatus(2)
         var response = await queryOrder(request);
         var res = JSON.parse(response.array[0])
+        if (!res.orderOrigins){
+            throw new Error("cannot find this order, is it closed?")
+        }
         var orderList = []
         for (var i = 0; i < res.orderOrigins.length; i++) {
             var obj = {}
