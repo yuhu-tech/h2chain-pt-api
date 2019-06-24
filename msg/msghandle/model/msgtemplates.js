f7719843aaab830dbb4161e1d0195c96c13c5ae7
lutianzhou001 <18015565550@189.cn>
2019-06-24 22:28:56 +0800

    change the templateid

--- msg/msghandle/model/msgtemplates.js
+++ msg/msghandle/model/msgtemplates.js
@@ -4,23 +4,23 @@
  */
 
 const HotelTIs = {
-    msgOne: 'gz7spWvbSmC6lGHeArydkK5oI_NDhm5Jw0_bPao_4xo',          //招聘动态提醒
-    msgTwo: 'to4Z0iqf1royHLMgHthnYGQ7TJTUWYj6533Cp3YHM4Q'           //订单关闭提醒
+    msgOne: 'MI6GUBVA5cNIm9TAB-sgt74WAT_bto4B4ByaFqEY3Io',          //招聘动态提醒
+    msgTwo: '9JQBLQ2kHsHyrcfVHWtWm7bvn9B673uG5fZcmDFM46I'           //订单关闭提醒
 }
 
 const AdviserTIs = {
-    msgOne: '-f8dEOOCDB_U9TRIZLoVTLHGAnXX6G1CdJH4dsBGmPA',           //新订单通知
-    msgTwo: 'XRnvbWgzYpiUJ8X0UcqOQqYgFY7E3tYRTiQgiy1qta8',          //招聘动态提醒
-    msgThree: '2GrS3K2yAVm9SVy_mln6blkwrxS89RIlTLY8O_Z8Fxk',        //新应聘通知
-    msgFour: 'd8eHEsZU5rgAl94wwUfSRCrniy-nJ_kEBL3S8GzYZRo',         //订单修改通知
-    msgFive: 'yoVpGciWBUC_FczM5Qw2ZG0V89WcpGfkORzNwJjVQfc',         //订单回复通知
-    msgSix: 'GLYoYDKp_t6f1lZ0yWizL2TdhjOugwYJ-nt4cIwULvs',        //订单关闭提醒
+    msgOne: 'b7Ijlx1hS7wXyuesuYLxzkb63jC9OAC2HsiircdUlwQ',           //新订单通知
+    msgTwo: 'K6xZitEvDhkVQ7MKue_ely9C6ASDzzWUxZZXOzB6nrY',          //招聘动态提醒
+    msgThree: 'QuuAwWJpq67TvNxfmn1IjVIzCGUSJ1P-ifvIEYHEcvs',        //新应聘通知
+    msgFour: '-5A3J40aNrQ2hIszptrig2PDBuQe4FajLFlzsNGpaPA',         //订单修改通知
+    msgFive: 'Udr-LYicBWjP32XuNUpD0jeOncwGWD1Xiyh1lA48r0U',         //订单回复通知
+    msgSix: 'kYpovHHJ4MqcE3OCPrMJepp6HOfCbIz1Y-lBu0jAkEg',        //订单关闭提醒
 }
 
 const PtTIs = {
-    msgOne:'brRgrT9zvQNCsL8PuMyMzFKnY1zrlAVIrsbP2LPGEj0',           //招聘动态提醒
-    msgTwo:'4CztLnLxJKiXCAYAeo5KzfkhOw77qlICee_hVEY1GNc',           //应聘落选提醒
-    msgThree:'lr-0FFSD85D4-ENW7coanxzS3195Tw_evmWY-01wnlI',         //订单关闭提醒
+    msgOne:'gBqLuGbuDqFggypeoYm9lNyGKZCYuZgJHuDEyT-HFqM',           //招聘动态提醒
+    msgTwo:'_DALmIcTVK4cSq65gP02PMYI99Y8CeRxVkSr8KZUKFI',           //应聘落选提醒
+    msgThree:'U90edFc3IueYp2ckIYzCn7n3sAx7CnsRQvxzexcWrLk',         //订单关闭提醒
 }
 
 
