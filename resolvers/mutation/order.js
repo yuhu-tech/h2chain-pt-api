298c5d96515b7bf27f80ea258ec29a610ade6832
lutianzhou001 <lutianzhou@yuhu.tech>
2019-07-09 19:57:06 +0800

    formatted

--- resolvers/mutation/order.js
+++ resolvers/mutation/order.js
@@ -8,30 +8,34 @@ const formid = require('../../msg/msghandle/formid/redis')
 const client = new services.MutationClient(config.localip, grpc.credentials.createInsecure());
 const order = {
     async registerorder(parent, args, ctx, info) {
-        var request = new messages.RegistryRequest();
-        request.setOrderid(args.registerorder.orderid);        //报名订单的id
-        const id = getUserId(ctx)
-        var personalmsgs = await ctx.prismaClient.personalmsgs({ where: { user: { id: id } } })
-        if (personalmsgs[0].name == null || personalmsgs[0].name == undefined || personalmsgs[0].name == '') {
-            throw new Error('cannot register order without making personalmessages')
+        try {
+            var request = new messages.RegistryRequest();
+            request.setOrderid(args.registerorder.orderid);        //报名订单的id
+            const id = getUserId(ctx)
+            var personalmsgs = await ctx.prismaClient.personalmsgs({ where: { user: { id: id } } })
+            if (personalmsgs[0].name == null || personalmsgs[0].name == undefined || personalmsgs[0].name == '') {
+                throw new Error('cannot register order without making personalmessages')
+            }
+            request.setPtid(id);
+            request.setAdviserid('001');
+            var myDate = new Date();
+            var now = myDate.getTime() / 1000
+            request.setApplytime(math.round(now));                       //报名时间，精确到秒
+            request.setSignintime(-1);                          //签到时间
+            request.setPtstatus(args.registerorder.register);                       //PT状态，后端已经写好了第一次报名确认后，状态：1    
+            request.setInviterid(args.registerorder.inviterid); //设置推荐人渠道
+            request.setType(args.registerorder.type); //设置报名类型 1:自有报名 2:顾问分享报名 3:代理分享报名
+            await client.registryOrder(request, function (err, response) { if (response.array[0] == 2) { throw new Error("Duplicate time") });
+            // set formid which is created when pt registry order
+            var userId = id
+            var orderId = args.registerorder.orderid
+            var formId = args.formid
+            var setRes = await formid.setFormId(userId, orderId, formId)
+            console.log('set formid after registrying :', setRes)
+            return true
+        } catch (error) {
+            throw (error)
         }
-        request.setPtid(id);
-        request.setAdviserid('001');
-        var myDate = new Date();
-        var now = myDate.getTime() / 1000
-        request.setApplytime(math.round(now));                       //报名时间，精确到秒
-        request.setSignintime(-1);                          //签到时间
-        request.setPtstatus(args.registerorder.register);                       //PT状态，后端已经写好了第一次报名确认后，状态：1    
-        request.setInviterid(args.registerorder.inviterid); //设置推荐人渠道
-        request.setType(args.registerorder.type); //设置报名类型 1:自有报名 2:顾问分享报名 3:代理分享报名
-        await client.registryOrder(request, function (err, response) { if (response.array[0] == 2) { throw new Error("Duplicate time") });
-        // set formid which is created when pt registry order
-        var userId = id
-        var orderId = args.registerorder.orderid
-        var formId = args.formid
-        var setRes = await formid.setFormId(userId, orderId, formId)
-        console.log('set formid after registrying :', setRes)
-        return true
     },
 
     async modifyptoforder(parent, args, ctx, info) {
